8d9d363e1f61dd922b0217a111d05b7314e67061
Abhinav Arya <abhinav.arya@cerner.com>
2020-03-17 21:45:18 +0530

    Updates

---
+++ precache-manifest.652ab946ae7fe5bae585a8aa07948dcc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b42163dcc7bc09c9c42e73bc0e6280",
+    "url": "/int_weather_app/index.html"
+  },
+  {
+    "revision": "6f8962a7a08230b0207f",
+    "url": "/int_weather_app/static/css/2.b554d19b.chunk.css"
+  },
+  {
+    "revision": "dbbee7cbf970d02319a3",
+    "url": "/int_weather_app/static/css/main.5be1ca7c.chunk.css"
+  },
+  {
+    "revision": "6f8962a7a08230b0207f",
+    "url": "/int_weather_app/static/js/2.8f7c9741.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/int_weather_app/static/js/2.8f7c9741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbee7cbf970d02319a3",
+    "url": "/int_weather_app/static/js/main.1f2a9333.chunk.js"
+  },
+  {
+    "revision": "4c5e87c38b8dd993e30d",
+    "url": "/int_weather_app/static/js/runtime-main.cdbea6ca.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/int_weather_app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/int_weather_app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/int_weather_app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/int_weather_app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/int_weather_app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "64d252560740d5a68bab9afcd6b25117",
+    "url": "/int_weather_app/static/media/search.64d25256.svg"
+  }
+]);
